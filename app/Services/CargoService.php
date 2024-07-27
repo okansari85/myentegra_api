9af8245d039ec27e27928112cb7f6d3d410748c4
okansari85 <sari_okan@hotmail.com>
2024-07-27 15:43:15 +0300

    sdasd

--- app/Services/CargoService.php
+++ app/Services/CargoService.php
@@ -30,9 +30,7 @@ class CargoService implements ICargo
                 'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
                 'origin' => 'https://www.n11.com',
                 'accept-encoding: gzip, deflate, br',
-            ],
-            'decode_content' => false,
-
+            ]
             ]);
 
 
