e016ff43d0fc738d9355c75aef16642e1e6ffa0d
okansari85 <sari_okan@hotmail.com>
2024-07-27 15:39:05 +0300

    asda

--- app/Services/CargoService.php
+++ app/Services/CargoService.php
@@ -29,7 +29,10 @@ class CargoService implements ICargo
                 'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
                 'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
                 'origin' => 'https://www.n11.com',
-                ]
+                'accept-encoding: gzip, deflate, br',
+            ],
+            'decode_content' => false,
+
             ]);
 
 
