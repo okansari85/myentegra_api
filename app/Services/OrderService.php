6fba31393dc3754a222a9e6eb07f4028d88f22ed
okansari85 <sari_okan@hotmail.com>
2024-09-05 11:03:00 +0300

    asdasd

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -12,9 +12,7 @@ class OrderService implements IOrder
 {
     public function getAllOrders($search,$per_page,$status){
 
-        if ($status=="1"){
-        $status = [$status,5];
-        }
+
 
         return Orders::with('items.orderable','buyer.adresses','items.product.coverImage','items.product.category.descendants')
             ->where(function ($query) use ($search,$status) {
