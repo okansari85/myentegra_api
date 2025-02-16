11fc01266926ee980ac1dccd2a537a3d35f2e11e
okansari85 <sari_okan@hotmail.com>
2025-02-16 19:59:55 +0300

    user_agent_change

--- app/Services/BotService.php
+++ app/Services/BotService.php
@@ -19,7 +19,7 @@ class BotService
         // Seçilen user-agent'ı headers'a ekleme
         $this->_parameters = [
             'headers' => [
-                'accept' => 'application/json',
+                'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
                 'user-agent' => $randomUserAgent,
             ]
         ];
