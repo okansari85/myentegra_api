129c7fc009702d41c7e74ce54b94e11d54e2a19b
okansari85 <sari_okan@hotmail.com>
2025-02-16 20:02:58 +0300

    change accept_ parameter

--- app/Services/BotServices/IlacFiyatiService.php
+++ app/Services/BotServices/IlacFiyatiService.php
@@ -23,7 +23,12 @@ class IlacFiyatiService extends BotService implements IIlacFiyati
             $url = self::END_POINT . '/ilaclar' . ($page ? "?pg=$page" : '');
 
             // GET isteğini yap
-            $response = $this->_client->request('GET', $url);
+            $response = $this->_client->request('GET', $url, [
+                'headers' => [
+                  'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
+                  'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+                ]
+              ]);
 
             // Yanıtın içeriğini JSON olarak döndür
             return $response->getBody()->getContents();
