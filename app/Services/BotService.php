fb72949a2a3fcbfa174568bbaca2c259e3b5901a
okansari85 <sari_okan@hotmail.com>
2025-02-16 20:12:31 +0300

    asdasd

--- app/Services/BotService.php
+++ app/Services/BotService.php
@@ -20,9 +20,27 @@ class BotService
         $this->_parameters = [
             'headers' => [
                 'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'user-agent' => $_SERVER['HTTP_USER_AGENT'],
+                'user-agent' => $randomUserAgent,
             ]
         ];
+
+
+        $this->_parameters = [
+            'headers' => [
+                'User-Agent' => $randomUserAgent,
+                'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'Accept-Encoding' => 'gzip, deflate, br',
+                'Accept-Language' => 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+                'Referer' => 'https://www.google.com/', // Kaynak siteyi Google gibi gösteriyoruz
+                'Connection' => 'keep-alive',
+                'Upgrade-Insecure-Requests' => '1',
+                'Cache-Control' => 'max-age=0',
+            ],
+            'cookies' => true, // Çerezleri aktif eder
+            'verify' => false, // SSL hatalarına karşı devre dışı bırakır (Güvenlik riskli olabilir)
+        ];
+
+
 
         $this->_client  = new Client($this->_parameters);
 
