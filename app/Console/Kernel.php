bb83ab2242134ee209a2f8762f7c9bd49682722b
okansari85 <sari_okan@hotmail.com>
2025-04-01 00:41:01 +0300

    kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
          $schedule->command('update:hb-orders-delivered')->everyFiveMinutes();
          $schedule->command('update:hb-orders-cancelled')->everyFiveMinutes();
         // $schedule->command('fetch:ilaclar')->everyFiveMinutes();
-
+         $schedule->command('update:hb-listing')->everyFifteenMinutes();
          $schedule->command('check:bosch-products')->twiceDaily(1, 13);
          $schedule->command('queue:restart')->everyFiveMinutes();
          $schedule->command('queue:work')->name('queue_work_name')->withoutOverlapping()->runInBackground();
