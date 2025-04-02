4acc73f76f1afc2e5b9214b73590849e968b8a91
OKAN SARI <sari_okan@hotmail.com>
2025-04-02 14:53:26 +0300

    updatehbstocks

--- app/Console/Commands/UpdateHbStocks.php
+++ app/Console/Commands/UpdateHbStocks.php
@@ -6,34 +6,47 @@ use Illuminate\Console\Command;
 
 use App\Models\Products;
 
+
+use App\Interfaces\IHBApi\IListing;
+
 class UpdateHbStocks extends Command
 {
 
     protected $signature = 'update:hb-stocks';
     protected $description = 'Supplier id 1 olan hb stocklarını günceller';
 
+    private IListing $listingService;
+
     /**
      * Execute the console command.
      */
-    public function handle()
+    public function handle(IListing $_listingService)
     {
-        //
+
+        $this->listingService = $_listingService;
 
         $data = Products::with('hb_product.hb_listing')
-        ->where('supplier_id', 1) // supplier_id = 1 olanları filtrele
-        ->whereHas('hb_product.hb_listing') // hb_listing ilişkisi olanları filtrele
+        ->where('supplier_id', 1)
+        ->whereHas('hb_product.hb_listing')
         ->get()
-        ->map(function ($product) {
-            $hb_listing = $product->hb_product->first()->hb_listing ?? null;
-            return [
-                'hepsiburadaSku' => $hb_listing->hepsiburada_sku ?? null,
-                'merchantSku' => $hb_listing->merchant_sku ?? null,
-                'availableStock' => $product->stock ?? 0,
-                'maximumPurchasableQuantity' => 0
-            ];
+        ->flatMap(function ($product) {
+            return $product->hb_product->map(function ($hb_product) use ($product) {
+                $hb_listing = $hb_product->hb_listing ?? null;
+
+                return [
+                    'hepsiburadaSku' => $hb_listing->hepsiburada_sku ?? null,
+                    'merchantSku' => $hb_listing->merchant_sku ?? null,
+                    'availableStock' => $product->stock ?? 0,
+                    'maximumPurchasableQuantity' => 0
+                ];
+            });
         })->toArray();
 
         print_r($data);
+
+        if (!empty($data)) {
+            $this->listingService->updateStock($data);
+        }
 
     }
 }
